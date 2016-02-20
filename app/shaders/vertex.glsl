937096780ba8df5ed3b9cfd459ab3256ddf78974
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-02-20 08:58:35 +0100

    Rotate the texture

--- app/shaders/vertex.glsl
+++ app/shaders/vertex.glsl
@@ -3,6 +3,13 @@ varying vec2 vUv;
 
 void main() {
   vUv = uv;
-  vec3 newPosition = position + normal * vec3(sin(time * 0.2) * 1.0);
+  vec3 newPosition;
+  float uCos = cos(time);
+  float uSin = sin(time);
+
+  newPosition.x = position.x * uCos - position.y * uSin;
+  newPosition.y = position.x * uSin + position.y * uCos;
+  newPosition.z = position.z + normal.z * sin(mod(time,2.0)) * 1.0;
+
   gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 }
