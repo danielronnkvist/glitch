5e21ea560c2b3c4103ce345315982e06ec1b9906
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-02-20 11:00:37 +0100

    Extracted noise to its own file

--- app/shader.js
+++ app/shader.js
@@ -1,19 +1,19 @@
 import { Vector2 } from 'three';
 var vertexShader = require('./shaders/vertex.glsl');
 var fragmentShader = require('./shaders/fragment.glsl');
+var noise = require('./shaders/noise.glsl');
 
 var shader = {
 
   uniforms: {
     time: { type: "f", value: 0 },
-    resolution: { type: "v2", value: new Vector2 },
     texture: { type: "t" },
     speed: { type: "f", value: 10.2 },
     distortion: { type: "f", value: 3.0 },
     distortion2: { type: "f", value: 10.0 }
   },
   vertexShader: vertexShader,
-  fragmentShader: fragmentShader
+  fragmentShader: noise + fragmentShader
 
 };
 
